142cc96ad45380289b19bd41110b10951d458aa5
AhmedNagy_Roul <64781251+AhmedRoul@users.noreply.github.com>
2023-03-03 18:54:15 +0200

    initial commit

---
+++ getNategaNadaAshraf_using_SSN/getNategaNadaAshraf_using_SSN/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Windows;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+namespace getNategaNadaAshraf_using_birthdate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Application.EnableVisualStyles();
+            Window w = new Window();
+            Application.Run(w);
+
+            new seleniumRun(w.SSN);
+
+
+        }
+
+    }
+    public class seleniumRun
+    {
+        IWebDriver driver;
+
+        public seleniumRun(double id)
+        {
+            driver=new ChromeDriver();
+            //driver=new OperaDriver();
+            //driver=new FirefoxDriver();
+// driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://tdb.tanta.edu.eg/eng_voting/default.aspx");
+            
+            driver.FindElement(By.Id("txt_ssid")).SendKeys(id.ToString());
+            for (int i = 0; i < 3000; i++)
+            {
+                driver.FindElement(By.Id("txt_ssid")).Clear();
+                driver.FindElement(By.Id("txt_ssid")).SendKeys(id.ToString());
+                id++;
+                driver.FindElement(By.Id("Button2")).Click();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(.65);
+
+
+                if(driver.FindElement(By.Id("lblSecurityCode")).Text!= "الرقم القومي الذي قمت بإدخاله غير صحيح")
+                {
+                    break;
+                }
+                Console.WriteLine("congratulation");
+                driver.Close();
+                //var d=driver.PageSource;
+                
+            }
+
+        }
+
+    }
+}
---
+++ getNategaNadaAshraf_using_SSN/getNategaNadaAshraf_using_SSN/Window.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace getNategaNadaAshraf_using_birthdate
+{
+    public partial class Window : Form
+    {
+        public double SSN=0;
+        public Window()
+        {
+            InitializeComponent();
+            dateTimePicker1.CustomFormat = "dd-MM-yyyy";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string date = dateTimePicker1.Text;
+            
+            var array_Date=date.Split('-');
+
+            string ssn = "";
+            //Date
+            if (int.Parse(array_Date[2]) > 1999)
+            {
+                ssn += "3";
+
+            }
+            else
+            {
+                ssn += "2";
+            }
+
+            ssn += array_Date[2][1].ToString()+ array_Date[2][0].ToString() + array_Date[1] + array_Date[0] + "2100000";
+            this.SSN = double.Parse(ssn);
+            Console.WriteLine(double.Parse( ssn));
+
+            Close();
+
+
+
+
+
+
+        }
+    }
+}
